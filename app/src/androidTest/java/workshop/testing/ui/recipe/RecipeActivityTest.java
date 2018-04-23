26568bd1c3e4e77b5b75667af5e50c6cf47b4d89
lionking2001 <kitsada_tang@cmu.ac.th>
2018-04-23 22:20:57 +0700

    Show detail of carrot

--- app/src/androidTest/java/workshop/testing/ui/recipe/RecipeActivityTest.java
+++ app/src/androidTest/java/workshop/testing/ui/recipe/RecipeActivityTest.java
@@ -16,20 +16,22 @@ import static workshop.testing.ui.recipe.RecipeActivity.KEY_ID;
 
 public class RecipeActivityTest {
     @Rule
-    public ActivityTestRule<RecipeActivity> activityTestRule = new ActivityTestRule<>(RecipeActivity.class, true, false);
+    public ActivityTestRule<RecipeActivity> activityRule
+            = new ActivityTestRule<RecipeActivity>(
+            RecipeActivity.class,true,false
+    );
 
-    @Test
-    public void openActivityByDefaultShouldShowRecipeRecipenotfound() {
-        activityTestRule.launchActivity(new Intent());
-        onView(withId(R.id.description)).check(matches(withText("Recipe not found")));
+//    @Test
+//    public void noContentOnOpenActivity(){
+//        onView(withId(R.id.description)).check(matches(withText("Recipe not found.")));
+//    }
 
-    }
-    
+
     @Test
     public void show_detail_of_chocolate_pudding() {
         Intent intent = new Intent();
         intent.putExtra(RecipeActivity.KEY_ID, "chocolate_pudding");
-        activityTestRule.launchActivity(intent);
+        activityRule.launchActivity(intent);
 
         onView(withId(R.id.title)).check(matches(withText("Chocolate Pudding")));
         onView(withId(R.id.description)).check(matches(withText("2 tablespoons chocolate\n" +
@@ -42,12 +44,23 @@ public class RecipeActivityTest {
                 "Cook until it thickens, beat whites of eggs and put on top, put in oven to brown. Serve with cream, if preferred.")));
 
     }
+    
+    @Test
+    public void show_detail_of_creamed_carrots(){
+        Intent intent=new Intent();
+        intent.putExtra(RecipeActivity.KEY_ID,"creamed_carrots");
+        activityRule.launchActivity(intent);
+
+        onView(withId(R.id.title)).check(matches(withText("Creamed Carrots")));
+        onView(withId(R.id.description)).check(matches(withText("Peel and dice carrots, cook in salt water until tender, remove from stove and drain. Fry 4 slides of bacon, drain off a part of grease, cut bacon in small slices, add carrots, 2 tablespoons of flour, stir all together, add 1 cup of sweet cream, salt and pepper to taste.")));
+
+    }
 
     @Test
     public void success() {
         Intent intent = new Intent();
         intent.putExtra(KEY_ID, "chocolate_pudding");
-        activityTestRule.launchActivity(intent);
+        activityRule.launchActivity(intent);
         onView(withId(R.id.title)).check(matches(withText("Chocolate Pudding")));
     }
 }
